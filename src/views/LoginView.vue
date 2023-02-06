30a41d65c8d5688bdd35d1d41962f38fa0a389cc
Helar Kärbo <helar.karbo@gmail.com>
2023-02-06 16:17:11 +0200

    Toytrade frontend added login page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-2">
+      <form>
+        <div class="mb-3">
+          <input type="text" class="form-control" placeholder="Kasutajanimi">
+        </div>
+        <div class="mb-3">
+          <input type="password" class="form-control" placeholder="Parool">
+        </div>
+        <button type="submit" class="btn btn-primary">Logi sisse</button>
+      </form>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "LoginView"
+}
+</script>
