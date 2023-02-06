b02d92ca891f1ee2dc348f7846decfd01c398b91
Helar Kärbo <helar.karbo@gmail.com>
2023-02-06 14:15:53 +0200

    Toytrade frontend first edit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Kõik töötab, võib koju minna!</h1>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
