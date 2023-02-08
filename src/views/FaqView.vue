a4a70c5526dcb9525b8903dfd867f043df1775d9
Valter Aro <valteraro@gmail.com>
2023-02-08 14:03:16 +0200

    Front KKK stuff

---
+++ src/views/FaqView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="text-align: center;">
+    <div style="display: inline-block; text-align: left;">
+      <br><br>
+      <h3>Korduma kippuvad küsimused</h3> <br>
+      Registreerimisel saad 3 vahetuspunkti.<br>
+      Iga edukalt ära antud asja eest saad juurde 1 vahetuspunkti.<br>
+      Iga asi maksab ühe vahetuspunkti.<br>
+      Kasutajal on võimalik ühes kuus kasutada ära maksimaalselt 5 punkti.<br>
+      Asju saab tellida ainult pakiautomaati.<br>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FaqView"
+}
+</script>
+
+<style scoped>
+
+</style>
