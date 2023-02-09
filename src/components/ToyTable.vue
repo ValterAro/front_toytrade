fa5089956e8027f33bd27b656f13cfe825cc5210
Helar Kärbo <helar.karbo@gmail.com>
2023-02-09 09:39:42 +0200

    Toytrade filtering works

--- src/components/ToyTable.vue
+++ src/components/ToyTable.vue
@@ -7,12 +7,12 @@
         <th scope="col">Kirjeldus</th>
         <th scope="col">Kategooria</th>
         <th scope="col">Seisukord</th>
-        <th scope="col">Asuoht</th>
+        <th scope="col">Asukoht</th>
         <th scope="col">Pilt</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="toy in toys">
+      <tr v-for="toy in toys" class="text-start">
         <td>{{ toy.name }}</td>
         <td>{{ toy.description }}</td>
         <td>{{ toy.categoryName }}</td>
@@ -29,6 +29,13 @@ export default {
   name: 'ToyTable',
   data: function () {
     return {
+      categories: [
+        {
+          categoryId: 0,
+          categoryName: '',
+          isSelected: false
+        }
+      ],
       toys: [
         {
           id: 0,
@@ -48,23 +55,40 @@ export default {
   },
   methods: {
     getToys: function () {
-      this.$http.get("/trade/trades", {
-            params: {
-              categoryId: this.categoryId,
-
-            }
-          }
+      this.$http.get("/trade/all"
       ).then(response => {
         this.toys = response.data
       }).catch(error => {
         console.log(error)
       })
     },
+    setCategoryFilters: function (selectedCategories) {
+      this.categories.forEach(category => category.isSelected = false)
+      for (let i = 0; i < this.categories.length; i++) {
+        for (let j = 0; j <= selectedCategories.length; j++) {
+          if (this.categories[i].categoryId === selectedCategories[j]) {
+            this.categories[i].isSelected = true;
+          }
+        }
+      }
+      this.$http.post("/trade/trades", this.categories
+      ).then(response => {
+        this.toys = response.data
+      }).catch(error => {
 
+      })
+    },getAllCategories: function () {
+      this.$http.get("/categories")
+          .then(response => {
+            this.categories = response.data
+          })
+          .catch(error => {
+          })
+    }
   },
   beforeMount() {
     this.getToys()
+    this.getAllCategories()
   }
-
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Kõik töötab, võib koju minna!</h1>
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
